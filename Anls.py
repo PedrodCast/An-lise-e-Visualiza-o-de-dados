28e8c0ef2f412ba4a3d64cdcfe134d775adcd223
PedrodCast <pedrodec.35@gmail.com>
2024-12-04 17:49:43 -0300

    Update Anls.py

--- Anls.py
+++ Anls.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 from itertools import combinations, product
 import millify as mils
+import re
 
 """ Inicializando o Dataset """
 
@@ -58,29 +60,39 @@ unique_genres = all_genres.unique()
 # Cria uma cópia do Dataframe original
 df_copy= df.copy()
 
+''' Análises com Gênero do filme'''
+
+# Separa os gêneros únicos para permitir uma análise de cada um deles
+# isso porque cada linha da coluna "genre" contém mais de um gênero
 df_copy['Genre'] = df_copy['Genre'].str.split(',')
-df_exp = df_copy.explode("Genre").drop_duplicates(subset='Gross', keep='first')
+df_exp = df_copy.explode("Genre")
+df_exp['Genre'] = df_exp['Genre'].apply(lambda x: re.sub(r'\s+', ' ', x.strip()))
 
 # Esse bloco de código cria uma tabela com arrecadação média por gênero
 gross_p_genre = df_exp.groupby('Genre')['Gross'].mean().reset_index()
 gross_p_genre = gross_p_genre.sort_values(by='Gross', ascending=True)
 
 # Esse bloco de código cria uma tabela com duração média por gênero
-runtime_p_genre = df_exp.groupby('Genre')['Runtime'].mean().reset_index()
+runtime_p_genre = df_exp.groupby(['Series_Title', 'Genre'])['Runtime'].mean().reset_index().groupby('Genre')['Runtime'].mean().reset_index()
 runtime_p_genre = runtime_p_genre.sort_values(by='Runtime', ascending=True)
 
 # Esse bloco de código cria uma tabela com ano mais comum de lançamento por gênero
 year_p_genre = df_exp.groupby('Genre')['Released_Year'].agg(lambda x: x.mode()[0]).reset_index()
 year_p_genre = year_p_genre.sort_values(by='Released_Year', ascending=True)
 
+# Esse bloco de código cria uma tabela com ano médio de lançamento por gênero
+year_p_genre_M = df_exp.groupby('Genre')['Released_Year'].mean().reset_index().round(0)
+year_p_genre_M = year_p_genre_M.sort_values(by='Released_Year', ascending=True)
+
 # Lista com a quantidade de cada gênero
 genre_counts = all_genres.value_counts()
 genre_counts = genre_counts.sort_values(ascending=True)
 
+''' Análises com data de lançamento do filme'''
 
+# Cria intervalos de 10 anos, começando em 1920 e indo até 2030
+bins = range(1920, 2030, 10)
 
-
-
-
-
-
+# Lista com a quantidade de cada ano
+year_counts = pd.cut(df['Released_Year'], bins=bins, include_lowest=True, right=False).value_counts()
+year_counts = year_counts.sort_index() #Ordena a lista
